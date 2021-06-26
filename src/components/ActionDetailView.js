3907d66a06187924d549a304e8e7f13eceb3046f
bohemeyer <bohemeyer@users.noreply.github.com>
2021-06-26 16:27:57 +0200

    Update ActionDetailView.js

--- src/components/ActionDetailView.js
+++ src/components/ActionDetailView.js
@@ -149,7 +149,9 @@ function ActionDetailView(props) {
     }
 
     return (
-      <div>Sie können immer nur 3 Maßnahme gleichzeitig durchführen. Sobald eine Maßnahme fertig ist, können Sie eine weitere auf den Weg bringen.</div>
+      <div>
+        Sie können immer nur 3 Maßnahme gleichzeitig durchführen. Sobald eine Maßnahme fertig ist, können Sie eine weitere auf den Weg bringen.
+      </div>
       <div className="action-detail-view__buttons">
         <button className="button button--secondary" onClick={onBackClick}>
           Zurück
